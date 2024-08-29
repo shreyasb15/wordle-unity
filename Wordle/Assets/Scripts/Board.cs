813ba2084217d953f7065293566b7a65ad6ad4fd
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2024-08-30 00:49:30 +0530

    game logic done

--- Wordle/Assets/Scripts/Board.cs
+++ Wordle/Assets/Scripts/Board.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using TMPro;
 
 public class Board : MonoBehaviour
 {
@@ -17,20 +21,69 @@ public class Board : MonoBehaviour
 
     private Row[] rows;
 
+    private string[] solutions;
+    private string[] validWords;
+    private string word;
+
     private int rowIndex;
     private int columnIndex;
+
+    [Header("States")]
+    public Tile.State emptyState;
+    public Tile.State occupiedState;
+    public Tile.State correctState;
+    public Tile.State wrongSpotState;
+    public Tile.State incorrectState;
+
+    [Header("UI")]
+    public TextMeshProUGUI invalidWordText;
 
     private void Awake()
     {
         rows = GetComponentsInChildren<Row>();
     }
 
+    private void Start()
+    {
+        LoadData();
+        SetRandomWord();
+    }
+
+    private void LoadData()
+    {
+        TextAsset textFile = Resources.Load("official_wordle_all") as TextAsset;
+        validWords = textFile.text.Split('\n');
+
+        textFile = Resources.Load("official_wordle_all") as TextAsset;
+        solutions = textFile.text.Split('\n');
+    }
+
+    private void SetRandomWord()
+    {
+        word = solutions[Random.Range(0, solutions.Length)];
+        word = word.ToLower().Trim();
+    }
+
     private void Update()
     {
+        Row currentRow = rows[rowIndex];
 
-        if(columnIndex >= rows[rowIndex].tiles.Length)
+        if (Input.GetKeyDown(KeyCode.Backspace))
         {
-            // submit row..
+            columnIndex = Mathf.Max(columnIndex - 1, 0);
+
+            currentRow.tiles[columnIndex].SetLetter('\0');
+            currentRow.tiles[columnIndex].SetState(emptyState);
+
+            invalidWordText.gameObject.SetActive(false);
+        }
+        else if (columnIndex >= currentRow.tiles.Length)
+        {
+            // submit row...
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SubmitRow(currentRow);
+            }
         }
         else
         {
@@ -38,11 +91,104 @@ public class Board : MonoBehaviour
             {
                 if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
                 {
-                    rows[rowIndex].tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    currentRow.tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    currentRow.tiles[columnIndex].SetState(occupiedState);
+
                     columnIndex++;
                     break;
                 }
             }
-        }        
+        }
+    }
+
+    private void SubmitRow(Row row)
+    {
+        if(!IsValidWord(row.word))
+        {
+            invalidWordText.gameObject.SetActive(true);
+            return;
+        }
+
+        string remaining = word;
+
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+
+            if(tile.letter == word[i])
+            {
+                tile.SetState(correctState);
+
+                remaining = remaining.Remove(i, 1);
+                remaining = remaining.Insert(i, " ");
+            }
+            else if(!word.Contains(remaining))
+            {
+                tile.SetState(incorrectState);
+            }
+        }
+
+        for(int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+
+            if(tile.state != correctState && tile.state != incorrectState)
+            {
+                if(remaining.Contains(tile.letter))
+                {
+                    tile.SetState(wrongSpotState);
+
+                    int index = remaining.IndexOf(tile.letter);
+                    remaining = remaining.Remove(index, 1);
+                    remaining = remaining.Insert(index, " ");
+                }
+                else
+                {
+                    tile.SetState(incorrectState);
+                }
+            }
+        }
+
+        //for(int i = 0;i < row.tiles.Length; i++)
+        //{
+        //    Tile tile = row.tiles[i];
+
+        //    if(tile.letter == word[i])
+        //    {
+        //        // correct state
+        //        tile.SetState(correctState);
+        //    }
+        //    else if(word.Contains(tile.letter))
+        //    {
+        //        // wrong spot
+        //        tile.SetState(wrongSpotState);
+        //    }
+        //    else
+        //    {
+        //        // incorrect
+        //        tile.SetState(incorrectState);
+        //    }
+        //}
+
+        rowIndex++;
+        columnIndex = 0;
+
+        if(rowIndex >= rows.Length)
+        {
+            enabled = false;
+        }
+    }
+
+    private bool IsValidWord(string word)
+    {
+        for(int i = 0; i < validWords.Length;i++)
+        {
+            if(validWords[i] == word)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
